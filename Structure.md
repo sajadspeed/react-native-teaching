58503153805b22ed4dd290142cf3253f3c399c55
Sajad Momeni <sajad79sp79@gmail.com>
2022-07-26 13:22:49 +0430

    Session_1 | 1401/5/4 | 01:50

---
+++ Structure.md
@@ -0,0 +1,7 @@
+# JavaScript
+- History
+- JS Engine and NodeJS
+- Data Types
+- ES6, Arrow Functions, Spread Operator, ...
+- OOP
+- Modules
---
+++ index.js
@@ -0,0 +1,87 @@
+// let number = 125;
+// const name = "Ali";
+// let tmp = NaN;
+
+// let scores = [10, 18, 17, 20]; // Array
+
+// scores.forEach((value, i)=>{
+	
+// })
+
+// for (const item of scores) {
+// 	//console.log(item);
+// }
+
+// //
+
+// scores = {
+// 	math: 10,
+// 	sport: 18,
+// 	cemestery: 17,
+// 	avg: function () {
+		
+// 	},
+// 	network: {
+// 		pod1: 12,
+// 		pod2: 15
+// 	}
+// }
+// const key = "math";
+
+// console.log(scores[key]);
+// console.log(scores.math);
+
+// console.log(Object.keys(scores));
+
+// let man = { // 0X13
+// 	name: 'ali',
+// 	age: 22
+// };
+// let manC = {...man};
+// // manC = 0X13
+
+// console.log("Before Change", manC);
+
+// man.age = 25;
+
+
+// // Q
+// console.log(manC);
+
+// const doSomeThing = (name, family = "Momeni", age = 12)=> {
+// 	console.log(`Your name is ${name}`)
+// }
+
+// doSomeThing("ALI")
+
+// let man = { 
+// 	name: 'ali',
+// 	age: 22
+// };
+
+
+// for (const key in object) {
+// 	if (Object.hasOwnProperty.call(object, key)) {
+// 		const element = object[key];
+		
+// 	}
+// }
+
+// const avgScores = (...numbers) => {
+// 	for (const number of numbers) {
+// 		// Code
+// 	}
+// 	return 17;
+// }
+
+// console.log(avgScores());
+
+let array1 = [12, 15, 18, 20, 15, 10];
+
+let array2 = [12,13,10,10,10];
+
+
+let arrayMerge = [...array1, ...array2];
+
+console.log(arrayMerge);
+
